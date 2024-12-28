b523ae5e5b7ba13a606cee5e9dd40f517ffbe9bb
Newton <pewelconsult@gmail.com>
2024-12-28 07:14:46 +0000

    commited

--- src/app/components/pages/userprofile/userprofile.component.ts
+++ src/app/components/pages/userprofile/userprofile.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
+import { BaseUrl } from '../../../interfaces/classes/BaseUrl';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../../../interfaces/classes/User';
 
 @Component({
   selector: 'app-userprofile',
@@ -8,6 +11,29 @@ import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
   templateUrl: './userprofile.component.html',
   styleUrl: './userprofile.component.css'
 })
-export class UserprofileComponent {
+export class UserprofileComponent implements OnInit{
+  ngOnInit(): void {
+    this.getUser()
+  }
 
+  baseurl = new BaseUrl();
+  private http = inject(HttpClient)
+  user: User = new User()
+
+  private getAuthHeaders(): HttpHeaders {
+      const token = localStorage.getItem('auth_token'); // or get from auth service
+      return new HttpHeaders({
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      });
+    }
+    
+
+  getUser() {
+    const headers = this.getAuthHeaders();
+    this.http.get(this.baseurl.url + "user", { headers }).subscribe((res: any) => {
+        console.log(res)
+        this.user = res
+    });
+  }
 }
---
+++ src/app/interfaces/classes/User.ts
@@ -0,0 +1,11 @@
+export class User {
+    fullName: string = "";
+    email: string ="";
+    phoneNumber: string ="";
+    dateOfBirth: string ="";
+    role: string ="";
+    company: string ="";
+    companyId: string = "";
+    createdOn?: string="";
+    createdBy: string ="";
+}
