c242b3f347fe43c3e3bcfb1e412f90206674f7c7
Newton <pewelconsult@gmail.com>
2024-12-28 04:30:40 +0000

    commit

--- src/app/interfaces/classes/BaseUrl.ts
+++ src/app/interfaces/classes/BaseUrl.ts
@@ -1,7 +1,7 @@
 export class BaseUrl {
     url: string
     constructor () {
-        this.url = "http://localhost:3000/"
-        //this.url = "https://rukinixbackend-140212836374.us-central1.run.app/"
+        //this.url = "http://localhost:3000/"
+        this.url = "https://rukinixbackend-140212836374.us-central1.run.app/"
     }
 }
